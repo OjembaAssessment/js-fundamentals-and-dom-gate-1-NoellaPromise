1a7cafb9b7df24abf34ad5d2b173cb3c1063ea31
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2023-07-24 13:04:21 +0200

    Add first question

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,12 @@
-console.log("Exercise 1 - Word Highlighter Noella");
+// to highlight a word we use <mark></mark> tag
+// 
+
+function highlight(text) {
+    let inputText = document.getElementById("myparagraph");
+    let innerHTML = inputText.innerHTML;
+    var index = innerHTML.indexOf(text);
+    if (index >= 0) { 
+     innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+     inputText.innerHTML = innerHTML;
+    }
+  }
